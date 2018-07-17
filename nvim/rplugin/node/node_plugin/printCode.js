ee1b591600e3083e0d23c3823b0af93a5ddc3907
gosu <bestgo82@gmail.com>
2018-07-17 23:50:04 +0900

    ing

--- nvim/rplugin/node/node_plugin/printCode.js
+++ nvim/rplugin/node/node_plugin/printCode.js
@@ -1,13 +1,56 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+
+const cp = require("child_process");
+const http = require("http");
+
+
+
 let printCode = async (plugin)=>{
 	try {
-		const buffer = await plugin.nvim.buffer;
-		const lines = await buffer.lines;
 
-		buffer.setLines("after gosugosu", {
-			start: 0,
-			end: -1,
-			strictIndexing: true,
-		});
+		const buffer = await plugin.nvim.buffer;
+		const _lines = await buffer.lines;
+
+		const _curDir = path.join(process.env.localappdata,"nvim","rplugin","node","node_plugin","views");
+		const _port = "1515";
+		const _url = "http://localhost:"+_port;
+
+		function activateServer(){
+			let app = express();
+			app.set("view engine","ejs");
+			app.set("views",_curDir);
+
+			app.get("/",defaultHandler);
+			app.use(express.static("ace-builds"));
+			let server = app.listen(_port);
+
+			return server;
+		}
+
+		const defaultHandler = (request,response)=>{
+			//response.json(_lines.join("\n"));
+			let codes=[];
+			codes.push(buffer.toString());
+			//response.render('index',{codes})
+			response.json(buffer.toString());
+		};
+
+		function executeBrowser(){
+			cp.exec("start "+_url);
+		}
+
+		let server = activateServer();
+		executeBrowser();
+		setTimeout(()=>{
+			server.close();
+		},3000);
+
+
+
+
 	} catch (err) {
 		console.error(err);
 	}
