4ac1df2307a2ae8d6a2a749724459f99357c6929
bestgo82 <bestgo82@gmail.com>
2018-07-17 17:07:33 +0900

    ing

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-function app(){
-	let dirLocalAppData = process.env.localappdata;
-	console.log(path.join(dirLocalAppData,"nvim","init.vim"));
+const copydir = require("copy-dir");
+let _dirNvim = path.join(__dirname,"nvim")
+let _dirLocalAppData = path.join(process.env.localappdata,"nvim");
 
-	let contentInitVim = fs.readFileSync("init.vim","utf-8");
-	console.log(contentInitVim);
-	
-	//
-	writeNvim(dirLocalAppData,contentInitVim);
-	//writeNvim("C:\\init.vim",contentInitVim);
-}
-
-function writeNvim(path,content){
-	fs.writeFileSync(path,content,"utf-8");
-}
-
-app();
+copydir.sync(_dirNvim,_dirLocalAppData);
 
